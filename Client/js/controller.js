274fc5ff91da3377b8b494458f9a9c24a4706fc4
Kevin Rodrigues <kevinrodrigues43@gmail.com>
2023-11-26 14:54:06 +0530

    Basic UI Changes 2

--- Client/js/controller.js
+++ Client/js/controller.js
@@ -20,6 +20,20 @@ document
     document.querySelector(".toggle_input").classList.add("expand");
   });
 
+document.addEventListener("click", (e) => {
+  let ele = document.querySelector(".toggle_input");
+  if (!document.querySelector(".new_task_container").contains(e.target)) {
+    if (ele.classList.contains("expand")) {
+      ele.classList.remove("expand");
+    }
+  }
+});
+
+document.querySelector(".menu_btn").addEventListener("click", () => {
+  let side_menu = document.querySelector(".side_menu_container");
+  side_menu.classList.toggle("side_expand");
+  document.querySelector(".menu_text").classList.toggle("menu_text");
+});
 /* sideNavMenuBtn.addEventListener("click", (e) => {
   document.querySelector(".side_menu").classList.add("expand_side");
 }); */
--- Client/js/index.js
+++ Client/js/index.js
@@ -1,9 +1,1 @@
-"use strict";
 
-/* 
-function removeCard(e) {
-  let cardId = e.target.parentElement.parentElement.id;
-  console.log(cardId);
-  document.getElementById(cardId).remove();
-}
- */
