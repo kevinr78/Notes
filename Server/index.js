e0b657712ba287d89d2d3d9fd9bfdf4ab780e569
Kevin Rodrigues <kevinrodrigues43@gmail.com>
2023-12-30 12:31:07 +0530

    Refactored Server Folders (Added routing, controllers)

--- Server/index.js
+++ Server/index.js
@@ -9,6 +9,8 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
+const noteRoute = require("./routes/note.route");
 /* connection.ConnectDb(); */
 /* app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,94 +22,23 @@ app.use(cors());
   );
   next();
 }); */
-app.get("/", (req, res) => {
+
+app.use("/note", noteRoute);
+/* app.get("/", (req, res) => {
   res.json("Hello");
 });
 
-app.post("/newTask", async (req, res) => {
-  let cleanedData = trimData(req.body);
-  let newNote = new Note(cleanedData);
-  try {
-    const note = await newNote.save();
-    res.json({
-      note,
-      ok: true,
-    });
-  } catch (error) {
-    res.json({
-      error,
-      ok: false,
-    });
-  }
+app.post("/newTask", async (req, res) => {});
+app.post("/loginUser", async (req, res) => {
+  console.log(req.body);
 });
 
-app.delete("/deleteNote", async (req, res) => {
-  let noteId = req.body.id.trim();
+app.delete("/deleteNote", async (req, res) => {});
 
-  try {
-    let deletedNote = await Note.findByIdAndDelete({ _id: noteId });
-    if (!deletedNote) {
-      throw new Error("Error while deleting note");
-    }
+app.post("/getNote", async (req, res) => {});
 
-    res.json({
-      deletedNote,
-      ok: true,
-    });
-  } catch (error) {
-    res.json({
-      error,
-      ok: false,
-    });
-  }
-});
+app.patch("/updateNote", async (req, res) => {}); */
 
-app.post("/getNote", async (req, res) => {
-  let noteId = req.body.id.trim();
-
-  try {
-    let note = await Note.findById({ _id: noteId });
-    if (!note) {
-      throw new Error("Error while fetching note");
-    }
-    note.Utitle = note.Utitle.trim();
-    note.Ubody = note.Ubody.trim();
-    note.Upriority = note.Upriority.toString().trim();
-
-    res.json({
-      note,
-      ok: true,
-    });
-  } catch (error) {
-    res.json({
-      error,
-      ok: false,
-    });
-  }
-});
-
-app.delete("/deleteTask", async (req, res) => {
-  try {
-    let note = await Note.deleteMany();
-    res.json({
-      note,
-      ok: true,
-    });
-  } catch (error) {
-    res.json({
-      error,
-      ok: false,
-    });
-  }
-});
-function trimData(Data) {
-  const { id, title, body, priority } = Data;
-  let Uid = Number(id.toString().trim());
-  let Utitle = title.trim();
-  let Ubody = body.trim();
-  let Upriority = Number(priority.toString().trim());
-  return { Uid, Utitle, Ubody, Upriority };
-}
 app.listen("3000", () => {
   console.log("Listening on port 3000");
 });
---
+++ Server/routes/note.route.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {
+  getUserNote,
+  createUserNote,
+  updateUserNote,
+  deleteUserNote,
+} = require("../controllers/notes");
+/* post -->/getNote ,/newTask, patch--> /updateNote , delete --> /deleteNote */
+
+router.route("/getNote").post(getUserNote);
+router.route("/newNote").post(createUserNote);
+router.route("/updateNote").patch(updateUserNote);
+router.route("/deleteNote").delete(deleteUserNote);
+
+module.exports = router;
